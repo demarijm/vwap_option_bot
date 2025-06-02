f0867bc3566e919e26297b9c3109beb10df1c4b1
Demari Miller <demarijmiller@gmail.com>
2025-06-02 19:03:10 -0400

    feat: connect frontend to backend and restructure

--- frontend/server/api/root.ts
+++ frontend/server/api/root.ts
@@ -1,8 +1,10 @@
 import { router } from './trpc'
 import { exampleRouter } from './routers/example'
+import { backendRouter } from './routers/backend'
 
 export const appRouter = router({
   example: exampleRouter,
+  backend: backendRouter,
 })
 
 export type AppRouter = typeof appRouter
